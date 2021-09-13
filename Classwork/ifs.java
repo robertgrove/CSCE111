8104f8b97bed0ecc1eb96110c939278a9e578120
robertgrove <robert.grove@tamu.edu>
2021-09-13 11:31:07 -0500

    created ifs.java

---
+++ Classwork/ifs.java
@@ -0,0 +1,26 @@
+/*
+Robert Grove
+UIN 628001368
+CSCE 111 Section 505
+9/8/2021
+Assignment 1
+*/
+
+class ifs
+{
+  public static void main(String[] args)
+  {
+    int a = 5;
+    int b = 4;
+    double c = .3;
+    double d = 3;
+    double e = c*d + .1;
+
+    if (e == 1.0)
+    {
+      System.out.println("We are in the if.");
+
+    }
+    System.out.println("We are done.");
+  }
+}
