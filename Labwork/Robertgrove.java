7cd53e169bf90b872c3bdfa803ddfa69835b97c0
robertgrove <robert.grove@tamu.edu>
2021-09-13 16:57:39 -0500

    started calander project

---
+++ Labwork/Robertgrove.java
@@ -0,0 +1,19 @@
+/*
+Robert Grove
+UIN 628001368
+CSCE 111 Section 505
+9/8/2021
+Assignment 1
+*/
+
+class prints
+{
+  public static void main(String[] args)
+  {
+    System.out.println("\t\t\t JANUARY \n");
+    System.out.println("------------------------------------\n");
+    System.out.println("|          |            |          |\n");
+    System.out.println("| Friday 1 | Saturday 2 | Sunday 3 |\n");
+
+  }
+}
