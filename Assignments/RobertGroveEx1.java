4608be984bdec2ce91c9b289d7b162eb319c2d21
robertgrove <robert.grove@tamu.edu>
2021-09-17 10:53:36 -0500

    finished assignment

---
+++ Assignments/RobertGroveEx1.java
@@ -0,0 +1,55 @@
+/*
+Robert Grove
+UIN 628001368
+CSCE 111 Section 505
+9/8/2021
+Assignment 1
+*/
+
+class prints
+{
+  public static void main(String[] args)
+  {
+    String month_0 = "\t\t======================================================================\n";
+    String month_1 = "\t\t|  /\\___ \\                                                        |\n";
+    String month_2 = "\t\t|  \\/__/\\ \\     __      ___   __  __     __     _ __   __  __      |\n ";
+    String month_3 = "\t\t|     _\\ \\ \\  /'__`\\  /' _ `\\/\\ \\/\\ \\  /'__`\\  /\\`'__\\/\\ \\/\\ \\       |\n ";
+    String month_4 = "\t\t|    /\\ \\_\\ \\/\\ \\L\\.\\_/\\ \\/\\ \\ \\ \\_\\ \\/\\ \\L\\.\\_\\ \\ \\/ \\ \\ \\_\\ \\      |\n ";
+    String month_5 = "\t\t|    \\ \\____/\\ \\__/.\\_\\ \\_\\ \\_\\ \\____/\\ \\__/.\\_\\\\ \\_\\  \\/`____ \\     |\n ";
+    String month_6 = "\t\t|     \\/___/  \\/__/\\/_/\\/_/\\/_/\\/___/  \\/__/\\/_/ \\/_/   `/___/> \\      |\n ";
+    String month_7 = "\t\t|                                                          /\\___/    |\n";
+    String month_8 = "\t\t|                                                          \\/__/     |\n";
+    String month_9 = "\t\t======================================================================\n\n";
+    String line_1 = "=================================================================================================== \n";
+    String line_2 = "|             |             |             |             |             | Saturday 01 |  Sunday 02  | \n";
+    String line_3 = "=================================================================================================== \n";
+    String line_4 = "|  Monday 03  | Tuesday 04  | Wedsnday 05 | Thursday 06 |  Friday 07  | Saturday 08 |  Sunday 09  | \n";
+    String line_5 = "=================================================================================================== \n";
+    String line_6 = "|  Monday 10  | Tuesday 11  | Wedsnday 12 | Thursday 13 |  Friday 14  | Saturday 15 |  Sunday 16  | \n";
+    String line_7 = "=================================================================================================== \n";
+    String line_8 = "|  Monday 17  | Tuesday 18  | Wedsnday 19 | Thursday 20 |  Friday 21  | Saturday 22 |  Sunday 23  | \n";
+    String line_9 = "=================================================================================================== \n";
+    String line_10 = "|  Monday 24  | Tuesday 25  | Wedsnday 26 | Thursday 27 |  Friday 28  | Saturday 29 |  Sunday 30  | \n";
+    String line_11 = "=================================================================================================== \n";
+    System.out.println(month_0);
+    System.out.println(month_1);
+    System.out.println(month_2);
+    System.out.println(month_3);
+    System.out.println(month_4);
+    System.out.println(month_5);
+    System.out.println(month_6);
+    System.out.println(month_7);
+    System.out.println(month_8);
+    System.out.println(month_9);
+    System.out.println(line_1);
+    System.out.println(line_2);
+    System.out.println(line_3);
+    System.out.println(line_4);
+    System.out.println(line_5);
+    System.out.println(line_6);
+    System.out.println(line_7);
+    System.out.println(line_8);
+    System.out.println(line_9);
+    System.out.println(line_10);
+  }
+}
--- Labwork/Robertgrove.java
+++ Labwork/Robertgrove.java
@@ -2,7 +2,7 @@
 Robert Grove
 UIN 628001368
 CSCE 111 Section 505
-9/8/2021
+9/16/2021
 Assignment 1
 */
 
@@ -10,10 +10,6 @@ class prints
 {
   public static void main(String[] args)
   {
-    System.out.println("\t\t\t JANUARY \n");
-    System.out.println("------------------------------------\n");
-    System.out.println("|          |            |          |\n");
-    System.out.println("| Friday 1 | Saturday 2 | Sunday 3 |\n");
-
+    System.out.println("Test\n");
   }
 }
